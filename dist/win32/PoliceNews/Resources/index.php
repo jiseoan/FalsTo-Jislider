3cff71828d3e098e656daeea1d0281d7d446a2c5
Han, geun-young <falsto@daum.net>
2014-06-27 14:37:39 +0900

    font anti-aliasing

---
+++ dist/win32/PoliceNews/Resources/index.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<head>
+	<title>JISLIDER - Police</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	<!-- page loading -->
+    <script type="text/javascript" src="js/pageloading.js"></script>
+    
+	<style type="text/css">
+    body {
+		background: #000;
+		margin: 0;
+		padding: 0;
+		overflow:hidden;
+	}
+	.container {
+		width: 100%;
+		margin: 0 auto; /* the auto value on the sides, coupled with the width, centers the layout */
+	}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div id="content1" name="content1" style="width:50%;float:left;"></div>
+        <div id="content2" name="content2" style="width:50%;float:left;"></div>
+    </div>
+</body>
